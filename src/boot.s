b7132818989906ff531c74b323a7e4cd93a66d99
Stephen Planck <splanck9271@gmail.com>
2022-11-26 21:22:09 -0500

    Bootloader now switches to protected mode

--- src/boot.s
+++ src/boot.s
@@ -1,27 +1,31 @@
-ORG 0
+ORG 0x7c00
 BITS 16
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
+
 _start:
     jmp short start
     nop
     
 times 33 db 0
 start:
-    jmp 0x7c0:step2 ; Set start of code segment
+    jmp 0:step2 ; Set start of code segment
     
 step2:
     cli ; Clear interupts
-    mov ax, 0x7c0
+    mov ax, 0x00
     mov ds, ax ; Set data segment to 0x7c0
     mov es, ax
-    mov ax, 0x00    
     mov ss, ax ; Set stack segment to zero
     mov sp, 0x7c00
     sti ; Enable interupts
+    
     mov si, message
     call print
     
-    jmp $
-
+    jmp load_protected
+    
 print:
     mov bx, 0
 .loop:
@@ -40,6 +44,54 @@ print_char:
 
 message: db 'THIS IS DEXTER!', 0
 
+load_protected:
+    cli
+    lgdt[gdt_descriptor]
+    mov eax, cr0
+    or eax, 0x1
+    mov cr0, eax
+    jmp CODE_SEG:load32
+
+gdt_start:
+gdt_null:
+    dd 0x0
+    dd 0x0
+
+gdt_code: ; Code segment
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x9a
+    db 11001111b
+    db 0
+
+gdt_data: ; Data segment
+    dw 0xffff
+    dw 0
+    db 0
+    db 0x92
+    db 11001111b
+    db 0
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+[BITS 32]
+load32:
+    mov ax, DATA_SEG
+    mov ds, ax
+    mov es, ax
+    mov fs, ax
+    mov gs, ax
+    mov ss, ax
+    mov ebp, 0x00200000
+    mov esp, ebp
+    
+    jmp $
+    
 ; Add the boot signature
 times 510-($ - $$) db 0
 dw 0xAA55
