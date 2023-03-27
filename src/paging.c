9055cf0589256793c1d4718a7b15dc6ee10c6f93
Stephen Planck <splanck9271@gmail.com>
2023-03-26 22:39:07 -0400

    now we have a 4gb chunk to play with!

--- src/paging.c
+++ src/paging.c
@@ -3,5 +3,21 @@
 
 struct paging_4gb_chunk* paging_new_4gb(uint8_t flags) {
     uint32_t* directory = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE);
-    
+    int offset = 0;
+
+    for(int i = 0; i < PAGING_TOTAL_ENTRIES_PER_TABLE; i++) {
+        uint32_t* entry = kzalloc(sizeof(uint32_t) * PAGING_TOTAL_ENTRIES_PER_TABLE); 
+        
+        for(int x = 0; x < PAGING_TOTAL_ENTRIES_PER_TABLE; x++) {
+            entry[x] = (offset + (x + PAGING_PAGE_SIZE)) | flags;
+        }
+
+        offset += (PAGING_TOTAL_ENTRIES_PER_TABLE * PAGING_PAGE_SIZE);
+        directory[i] = (uint32_t)entry | flags | PAGING_IS_WRITEABLE;
+    }
+
+    struct paging_4gb_chunk* chunk_4gb = kzalloc(sizeof(struct paging_4gb_chunk));
+    chunk_4gb->directory_entry = directory;
+
+    return chunk_4gb;
 }
