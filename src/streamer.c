1036b5811e29a766178321f744e2f5fe42dc8c56
Stephen Planck <splanck9271@gmail.com>
2023-04-21 00:24:49 -0400

    disk streamer reads bytes from disk into a buffer

--- src/streamer.c
+++ src/streamer.c
@@ -1,6 +1,7 @@
 #include "streamer.h"
 #include "heap.h"
 #include "kheap.h"
+#include "config.h"
 
 struct disk_stream* disk_streamer_new(int disk_id) {
     struct disk* disk = disk_get(disk_id);
@@ -17,4 +18,28 @@ int diskstreamer_seek(struct disk_stream* stream, int pos) {
     stream->pos = pos;
 
     return 0;
+}
+
+int disk_streamer_close(struct disk_stream* stream) {
+    kfree(stream);
+}
+
+int disk_streamer_read(struct disk_stream* stream, void* out, int total) {
+    int sector = stream->pos / DEXTER_SECTOR_SIZE, offset = stream->pos % DEXTER_SECTOR_SIZE;
+    int total_to_read = total > DEXTER_SECTOR_SIZE ? DEXTER_SECTOR_SIZE : total, res = 0;
+    char buf[DEXTER_SECTOR_SIZE];
+
+    res = disk_read_block(stream->disk, sector, 1, buf);
+    
+    if(res < 0) goto out;
+
+    for(int x = 0; x < total; x++)
+        *(char*)out++ = buf[offset + x];
+
+    stream->pos += total_to_read;
+    
+    if(total > DEXTER_SECTOR_SIZE)
+        res = disk_streamer_read(stream, out, total - DEXTER_SECTOR_SIZE);
+out:
+    return res;
 }
--- src/streamer.h
+++ src/streamer.h
@@ -10,5 +10,7 @@ struct disk_stream {
 
 struct disk_stream* disk_streamer_new(int disk_id);
 int diskstreamer_seek(struct disk_stream* stream, int pos);
+int disk_streamer_close(struct disk_stream* stream);
+int disk_streamer_read(struct disk_stream* stream, void* out, int total);
 
 #endif
