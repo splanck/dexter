988add3af9201a9cb11dab2d035210878878d18c
Stephen Planck <splanck9271@gmail.com>
2023-04-25 00:22:13 -0400

    Added some definitions for fat16

---
+++ images/README.md
@@ -0,0 +1,5 @@
+
+# Latest Boot image
+The latest Dexter bootable image can be found here. It will be updated with most new commits. This image has only been tested with the i386 version of the QEMU emulator, but may also work on other emulators such as Bochs or VirtualBox.
+
+
--- src/fs/fat16.h
+++ src/fs/fat16.h
@@ -3,7 +3,25 @@
 
 #include "../fs/file.h"
 
-struct filesystem* fat16_init();
+typedef unsigned int FAT_ITEM_TYPE;
 
+#define DEXTER_FAT16_SIGNATURE 0x29
+#define DEXTER_FAT16_FAT_ENTRY_SIZE 0x02
+#define DEXTER_FAT16_BAD_SECTOR 0xFF7
+#define DEXTER_FAT16_UNUSED 0x00
+
+#define FAT_ITEM_TYPE_DIRECTORY 0
+#define FAT_ITEM_TYPE_FILE 1
+#define FAT_FILE_READ_ONLY 0x01
+#define FAT_FILE_HIDDEN 0x02
+#define FAT_FILE_SYSTEM 0x04
+#define FAT_FILE_VOLUME_LABEL 0x08
+#define FAT_FILE_SUBDIRECTORY 0x10
+#define FAT_FILE_ARCHIVED 0x20
+#define FAT_FILE_DEVICE 0x40
+#define FAT_FILE_RESERVED 0x80
+
+
+struct filesystem* fat16_init();
 
 #endif
