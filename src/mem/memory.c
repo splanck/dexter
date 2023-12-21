d3d04554b8a2dcba53b173e44329e92e4756a5de
Stephen Planck <splanck9271@gmail.com>
2023-12-20 23:54:44 -0500

    Added memmove function to memory.c

--- src/mem/memory.c
+++ src/mem/memory.c
@@ -1,6 +1,38 @@
 #include "mem/memory.h"
 #include "mem/heap.h"
 
+void* memmove(void* dest, const void* src, size_t n) 
+{
+    unsigned char* d = dest;
+    const unsigned char* s = src;
+
+    // If the source and destination overlap, it could result in
+    // data corruption if we just copied the data normally. To
+    // avoid this, we need to decide whether to copy forwards
+    // or backwards.
+
+    if (d < s) 
+    {
+        // If the destination is before the source, we can copy
+        // forward safely.
+        for (size_t i = 0; i < n; i++) 
+        {
+            d[i] = s[i];
+        }
+    } 
+    else 
+    {
+        // If the destination is after the source, we need to copy
+        // backwards to prevent overwriting the data we want to copy.
+        for (size_t i = n; i != 0; i--) 
+        {
+            d[i - 1] = s[i - 1];
+        }
+    }
+
+    return dest;
+}
+
 void* memset(void* ptr, int c, size_t size)
 {
     char* c_ptr = (char*) ptr;
--- src/mem/memory.h
+++ src/mem/memory.h
@@ -3,7 +3,7 @@
 
 #include <stddef.h>
 
-int memmove(void *dest, const void *src, size_t n);
+void* memmove(void* dest, const void* src, size_t n);
 void* memset(void* ptr, int c, size_t size);
 int memcmp(void* s1, void* s2, int count);
 void* memcpy(void* dest, void* src, int len);
